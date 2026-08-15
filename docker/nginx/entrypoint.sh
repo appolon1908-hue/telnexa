ee6eb1fd101c26ed7449100be9ceb40e88b8917a
root <root@Ubuntu-jammy-latest-amd64-base.zst>
2026-08-15 14:14:55 +0200

    dockerize billing control plane

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
-export SMS_DOMAIN API_DOMAIN
+export SMS_DOMAIN API_DOMAIN PORTAL_DOMAIN ADMIN_DOMAIN
 if [ -s "/etc/letsencrypt/live/$SMS_DOMAIN/fullchain.pem" ] && [ -s "/etc/letsencrypt/live/$SMS_DOMAIN/privkey.pem" ]; then
-  envsubst '${SMS_DOMAIN} ${API_DOMAIN}' < /opt/telnexa/tls.conf.template > /etc/nginx/conf.d/default.conf
+  envsubst '${SMS_DOMAIN} ${API_DOMAIN} ${PORTAL_DOMAIN} ${ADMIN_DOMAIN}' < /opt/telnexa/tls.conf.template > /etc/nginx/conf.d/default.conf
 else
-  envsubst '${SMS_DOMAIN} ${API_DOMAIN}' < /opt/telnexa/http.conf.template > /etc/nginx/conf.d/default.conf
+  envsubst '${SMS_DOMAIN} ${API_DOMAIN} ${PORTAL_DOMAIN} ${ADMIN_DOMAIN}' < /opt/telnexa/http.conf.template > /etc/nginx/conf.d/default.conf
 fi
